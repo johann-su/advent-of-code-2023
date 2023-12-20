e039a36bd76202b21447b034094f777334ddb97d
joss478d <johann.suess1@mailbox.tu-dresden.de>
2023-12-20 22:35:03 +0100

    day 12

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,5 +1,3 @@
-use std::vec;
-
 use itertools::Itertools;
 
 fn task_1(input: &str) -> u32 {
@@ -9,40 +7,49 @@ fn task_1(input: &str) -> u32 {
             let (springs, nums): (&str, &str) = line.split(" ").collect_tuple().unwrap();
             let group_sizes: Vec<u32> = nums.split(",").map(|n| n.parse().unwrap()).collect_vec();
 
-            let mut all_combinations: Vec<String> = vec![springs.to_owned()];
+            let mut combinations: Vec<String> = vec![springs.to_string()];
 
-            while all_combinations.iter().any(|str| str.contains("?")) {
-                let str = all_combinations.remove(0);
-                if str.contains("?") {
-                    all_combinations.push(str.clone().replacen("?", ".", 1));
-                    all_combinations.push(str.clone().replacen("?", "#", 1));
-                } else {
-                    all_combinations.push(str);
-                }
+            while let Some((i, line)) = combinations.iter().find_position(|line| line.contains('?'))
+            {
+                let line = line.clone();
+                combinations.remove(i);
+                combinations.push(line.replacen("?", "#", 1));
+                combinations.push(line.replacen("?", ".", 1));
             }
 
-            all_combinations
-                .iter()
-                .filter(|line| {
-                    let mut num_groups: Vec<u32> = vec![];
-                    for (key, group) in &line.chars().group_by(|char| *char == '#') {
+            combinations.iter().fold(0, |acc, line| {
+                if line
+                    .chars()
+                    .group_by(|char| *char == '#')
+                    .into_iter()
+                    .filter_map(|(key, group)| {
                         if key == true {
-                            num_groups.push(group.collect_vec().len() as u32)
+                            Some(group.count() as u32)
+                        } else {
+                            None
                         }
-                    }
-
-                    num_groups == group_sizes
-                })
-                .collect_vec()
-                .len() as u32
+                    })
+                    .collect_vec()
+                    == group_sizes
+                {
+                    acc + 1
+                } else {
+                    acc
+                }
+            })
         })
-        .sum()
+        .sum::<u32>()
+}
+
+fn task_2(input: &str) -> u32 {
+    0
 }
 
 fn main() {
     let input = include_str!("./data.txt");
 
     println!("task 1: {}", task_1(input));
+    println!("task 2: {}", task_2(input));
 }
 
 #[cfg(test)]
@@ -60,5 +67,18 @@ mod test {
 
         let result = task_1(input);
         assert_eq!(result, 21);
+    }
+
+    #[test]
+    fn task_2_works() {
+        let input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
+
+        let result = task_2(input);
+        assert_eq!(result, 525152);
     }
 }
