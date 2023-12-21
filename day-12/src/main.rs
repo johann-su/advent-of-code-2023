ee04e8a33b68cb7b74204e994c74b0a6cac40cd8
joss478d <johann.suess1@mailbox.tu-dresden.de>
2023-12-21 14:41:52 +0100

    day 12

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,48 +1,90 @@
+use std::iter;
+use cached::proc_macro::cached;
+
 use itertools::Itertools;
 
-fn task_1(input: &str) -> u32 {
+#[cached]
+fn count(str: String, groups: Vec<u32>) -> u64 {
+    if str == "" {
+        if groups.is_empty() {
+            // no springs left and expecting none
+            return 1;
+        } else {
+            // no springs left but expecting more
+            return 0;
+        }
+    }
+    if groups.is_empty() {
+        if str.contains('#') {
+            // expecting no more groups but str still contains groups
+            return 0;
+        } else {
+            // expecting no more groups and str doesnt contain any
+            return 1;
+        }
+    }
+    
+    let mut result = 0;
+
+    // case 1: operational spring
+    if ['.', '?'].contains(&str.chars().next().unwrap()) {
+        // skip operational spring and check for the rest of string
+        result += count(str.get(1..).unwrap().to_string(), groups.clone());
+    }
+
+    // case 2: broken spring
+    if ['#', '?'].contains(&str.chars().next().unwrap()) {
+        // only valid if:
+        // - there are enough springs left
+        // - the required block size can be achived (no '.' in first n chars of str)
+        // - spring after block must be '.' (either no springs left or next char is not '#')
+        if groups[0] as usize <= str.len()
+            && !str.get(..groups[0] as usize).unwrap().contains('.')
+            && (groups[0] as usize == str.len()
+                || str.chars().collect_vec()[groups[0] as usize] != '#')
+        {
+            // the block is valid -> remove n+1 chars from string (because after group there must be '.') and the first group size from groups
+            if let Some(str) = str.get((groups[0] + 1) as usize..) {
+                result += count(
+                    str.to_string(),
+                    groups[1..].to_vec(),
+                )
+            } else {
+                result += count(
+                    "".to_string(),
+                    groups[1..].to_vec(),
+                )
+            }
+        }
+    }
+
+    result
+}
+
+fn task_1(input: &str) -> u64 {
     input
         .lines()
         .map(|line| {
             let (springs, nums): (&str, &str) = line.split(" ").collect_tuple().unwrap();
             let group_sizes: Vec<u32> = nums.split(",").map(|n| n.parse().unwrap()).collect_vec();
 
-            let mut combinations: Vec<String> = vec![springs.to_string()];
-
-            while let Some((i, line)) = combinations.iter().find_position(|line| line.contains('?'))
-            {
-                let line = line.clone();
-                combinations.remove(i);
-                combinations.push(line.replacen("?", "#", 1));
-                combinations.push(line.replacen("?", ".", 1));
-            }
-
-            combinations.iter().fold(0, |acc, line| {
-                if line
-                    .chars()
-                    .group_by(|char| *char == '#')
-                    .into_iter()
-                    .filter_map(|(key, group)| {
-                        if key == true {
-                            Some(group.count() as u32)
-                        } else {
-                            None
-                        }
-                    })
-                    .collect_vec()
-                    == group_sizes
-                {
-                    acc + 1
-                } else {
-                    acc
-                }
-            })
+            count(springs.to_string(), group_sizes)
         })
-        .sum::<u32>()
+        .sum()
 }
 
-fn task_2(input: &str) -> u32 {
-    0
+fn task_2(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|line| {
+            let (springs, nums): (&str, &str) = line.split(" ").collect_tuple().unwrap();
+            let group_sizes: Vec<u32> = nums.split(",").map(|n| n.parse().unwrap()).collect_vec();
+            let springs = [springs; 5].join("?");
+            let group_sizes = iter::repeat(group_sizes).take(5).flatten().collect_vec();
+
+            count(springs, group_sizes) as u64
+        })
+        .sum()
 }
 
 fn main() {
